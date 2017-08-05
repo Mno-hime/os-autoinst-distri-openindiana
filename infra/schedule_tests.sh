256dc4e1df817ddc3c25a0d294d817a801b341b4
Michal Nowak <mnowak@startmail.com>
2017-08-05 19:47:52 +0200

    Indent with spaces, remove i386 support

--- infra/schedule_tests.sh
+++ infra/schedule_tests.sh
@@ -22,23 +22,21 @@ else
     for medium in iso usb; do
         for flavor in Minimal Server Live; do
             if [ "$medium" = "usb" ]; then flavor=${flavor}USB; fi
-            for arch in x86_64 i386; do
-                if [[ "$flavor" =~ "Live" && "$arch" = "i386" ]]; then continue; fi
-                if [[ "$flavor" =~ "Minimal" ]]; then mediumtype="minimal"; fi
-                if [[ "$flavor" =~ "Server" ]]; then mediumtype="text"; fi
-                if [[ "$flavor" =~ "Live" ]]; then mediumtype="gui"; fi
-                if [[ "$medium" = "usb" ]]; then
-                    mediumpath="../other/"
-                    mediumpathreal='other'
-                else
-                    mediumpath=""
-                    mediumpathreal='iso'
-                fi
-                filepath="${asset_root}/${mediumpathreal}/OI-hipster-${mediumtype}-${build}.${medium}"
-                if [ ! -f ${filepath} ]; then continue; fi
-                echo -ne "  $flavor\t$arch\t$medium\t"
-                $client isos post iso=${mediumpath}OI-hipster-${mediumtype}-${build}.${medium} DISTRI=openindiana VERSION=hipster FLAVOR=$flavor ARCH=$arch BUILD=$build "$filter"
-            done
+            arch=x86_64
+            if [[ "$flavor" =~ "Minimal" ]]; then mediumtype="minimal"; fi
+            if [[ "$flavor" =~ "Server" ]]; then mediumtype="text"; fi
+            if [[ "$flavor" =~ "Live" ]]; then mediumtype="gui"; fi
+            if [[ "$medium" = "usb" ]]; then
+                mediumpath="../other/"
+                mediumpathreal='other'
+            else
+                mediumpath=""
+                mediumpathreal='iso'
+            fi
+            filepath="${asset_root}/${mediumpathreal}/OI-hipster-${mediumtype}-${build}.${medium}"
+            if [ ! -f ${filepath} ]; then continue; fi
+            echo -ne "  $flavor\t$arch\t$medium\t"
+            $client isos post iso=${mediumpath}OI-hipster-${mediumtype}-${build}.${medium} DISTRI=openindiana VERSION=hipster FLAVOR=$flavor ARCH=$arch BUILD=$build "$filter"
         done
     done
 fi
